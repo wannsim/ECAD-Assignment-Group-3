0e5401ec8a3aee4fdc764074445928999fc42da6
Wan Sim Yee <s10242681@connect.np.edu.sg>
2025-01-14 17:32:02 +0800

    Design homepage: add in image and welcome shopper

--- index.php
+++ index.php
@@ -4,8 +4,20 @@ session_start();
 // Include the Page Layout header
 include("header.php"); 
 ?>
-<img src="Images/welcome2mamaya.jpg" class="img-fluid" 
-     style="display:block; margin:auto;"/>
+
+<h1 style="text-align: center; font-size: 4em; font-family: Arial, sans-serif;">
+     <img src="https://cdn-icons-png.flaticon.com/128/2568/2568424.png" alt="Baby Icon" 
+     style="vertical-align: middle; width: 80px; height: 80px; margin-right: 10px;">
+    BABY SHOP
+    <img src="https://cdn-icons-png.flaticon.com/128/2568/2568424.png" alt="Baby Icon" 
+    style="vertical-align: middle; width: 80px; height: 80px; margin-right: 10px;">
+</h1>
+
+<?php
+    echo "<i class='fa-solid fa-user' style='margin-left: 20px;'></i>
+          <span id='username' style='margin-left: 5px;'>WELCOME, ".$_SESSION['ShopperName']."</span>";
+?>
+
 
 <?php 
 // Include the Page Layout footer
