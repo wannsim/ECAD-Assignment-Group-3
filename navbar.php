d52bfb04ff3b396aad57c6ed628c26a64d4936b3
Ho Yi Victoria <s10243726@connect.np.edu.sg>
2025-01-14 22:23:14 +0800

    change to login and logout

--- navbar.php
+++ navbar.php
@@ -1,12 +1,3 @@
-<?php
-$content1="Guest";
-if(isset($_SESSION["ShopperName"])) { 
-	//To Do 1 (Practical 2) - 
-    //Display a greeting message, Change Password and logout links 
-    //after shopper has logged in.
-	$content1 = "$_SESSION[ShopperName]";
-}
-?>
  <header>
         <a href="" class="logo"><img src="Images/icon.png" alt="" style="width:80px;"></a>
         <div class="bx bx-menu" id="menu-icon" style="color:black"></div>
@@ -14,16 +5,16 @@ if(isset($_SESSION["ShopperName"])) {
         <ul class="navbar">
             <li><a href="index.php"><i class="fa-sharp fa-solid fa-house"></i><span>&nbsp;Home</span></a></li>
             <li><a href="category.php"><i class="fa-solid fa-magnifying-glass"></i><span>&nbsp;Product Category</span></a></li>
-            <li><a href="shoppingCart.php" id="cart-icon"><i class="fa-solid fa-cart-shopping"></i><span>&nbsp;Shopping Cart</span></a></li>
+            <li><a href="shoppingCart.php"><i class="fa-solid fa-cart-shopping"></i><span>&nbsp;Shopping Cart</span></a></li>
         </ul>
         <div class="profile">
             <!-- Create a dropdown navbar to change password, logout etc, past orders -->
             <?php
                 if (!isset($_SESSION["ShopperName"])){
-                    echo "<a href='login.php' id='logout'><i class='fa-solid fa-user'></i><span id='username'>Guest</span></a>";
+                    echo "<a href='login.php'><i class='fa-solid fa-right-to-bracket'></i><span id='username'>Login</span></a>";
                 }
                 else{
-                    echo "<a href='login.php' id='logout'><i class='fa-solid fa-user'></i><span id='username'>$_SESSION[ShopperName]</span></a>";
+                    echo "<a href='logout.php'><i class='fa-solid fa-right-from-bracket fa-flip-horizontal'></i><span id='username'>Logout</span></a>";
                 }
             ?>
         </div>
